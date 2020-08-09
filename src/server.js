79c4bb31ab2ff2983c6f2f09d16cdef019d79002
Yesman Choque <62268626+YesmanChoque@users.noreply.github.com>
2020-08-09 15:41:59 -0300

    Back-end e banco de dados funcionais
    Os dados dos formulários (site) são gravados no banco de dados. O back-end gerencia os dados e retorna ao site (quando houver pesquisa). Um sistema request e response

--- src/server.js
+++ src/server.js
@@ -1,77 +1,8 @@
-const proffys = [
-    {
-        name: "Diego Fernandes",
-        avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4",
-        whatsapp: "11979850777",
-        bio: "Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.",
-        subject: "Química",
-        cost: "20",
-        weekday: [0],
-        time_from: [720],
-        time_to: [1280]
-    },
-    {
-        name: "Jama!",
-        avatar: "https://avatars0.githubusercontent.com/u/61145881?s=460&u=c78376285d15e208b6adfd98a0fc9342de54fd6b&v=4",
-        whatsapp: "11979850777",
-        bio: "Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.",
-        subject: "Química",
-        cost: "20",
-        weekday: [1],
-        time_from: [720],
-        time_to: [1280]
-    },
-    {
-        name: "Respiration Fuedase",
-        avatar: "https://avatars1.githubusercontent.com/u/47800334?s=460&u=5428d4b5a12f2855349eb67af38ad0e758ab23bf&v=4",
-        whatsapp: "11979850777",
-        bio: "Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.",
-        subject: "Química",
-        cost: "20",
-        weekday: [1],
-        time_from: [720],
-        time_to: [1280]
-    }
-
-
-]
-
-const subjects = [
-    "Artes",
-    "Biologia",
-    "Ciências",
-    "Educação física",
-    "Física",
-    "Geografia",
-    "História",
-    "Matemática",
-    "Português",
-    "Química",
-]
-
-const weekdays = [
-    "Domingo",
-    "Segunda-feira",
-    "Terça-feira",
-    "Quarta-feira",
-    "Quinta-feira",
-    "Sexta-feira",
-    "Sábado",
-]
-
-
-
-function getSubject(subjectNumber){
-    const position = +subjectNumber - 1
-    return subjects[position]
-}
-
-
-
-
-
+//server
 const express = require("express")
 const server = express()
+
+const {pageLanding, pageStudy, pageGiveClasses, saveClasses} = require("./pages");
 
 //configurar nunjucks
 const nunjucks = require("nunjucks")
@@ -79,41 +10,20 @@ nunjucks.configure("src/views", {
     express: server,
     noCache: true,
 }
-
 )
 //================================//mudar de senFile para render e colocar apenas o nome do arquivo. O diretorio já foi especificado
 
-server.use(express.static("public"))
-.get("/", (req, res) => {
-    
-    return res.render("index.html")
-})
 
-.get("/study", (req, res) => {
-    const filters = req.query
-    return res.render("study.html", {proffys, filters, subjects, weekdays})//dps do arquivo coloque o objeto
-})
-
-.get("/give-classes", (req, res) => {
-    const datas = req.query
-
-
-    
-
-
-
-    const isNotEmpty = Object.keys(datas).length > 0
-    //Adicionar dados à lista de proffys
-    if (isNotEmpty) {
-        datas.subjects = getSubject(datas.subjects)
-        console.log("Dados Salvos!")
-        proffys.push(datas)
-        return res.redirect("/study")
-    } 
-    
-
-    return res.render("give-classes.html", {subjects, weekdays})
-})
-
-
+//incio e configuração do servidor
+server
+//receber os dados do req.body
+.use(express.urlencoded({extended: true}))
+//configurar arquivos estáticos (css, scripts, imagens)
+.use(express.static("public"))
+//rotas da aplicação
+.get("/", pageLanding)
+.get("/study", pageStudy)
+.get("/give-classes", pageGiveClasses)
+.post("/save-classes", saveClasses)
+//start do servidor na porta 5000. IP é o localhost
 .listen(5000)
